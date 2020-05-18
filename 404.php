343629085ef9f62a623548b19342c0b3ac79b0b3
numayama1111 <k.numayama1111@gmail.com>
2020-05-18 20:24:09 +0900

    コミット練習３

--- 404.php
+++ 404.php
@@ -1,22 +1,4 @@
 <?php get_header(); ?>
-<!-- errorSection -->
-<div class="errorSection">
-    <!-- inner -->
-    <div class="inner">
-        <div>
-            <figure class="errorImage">
-                <img src="<?php echo get_template_directory_uri(); ?>/./img/404.jpg" alt="エラー画像">
-            </figure>
-            <div class="errorText">
-                <p>Not Found</p>
-                <p>お探しのページが見つかりませんでした。</p>
-            </div>
-            <div class="topButton">
-                <p><a href="<?php echo home_url(); ?>">TOPへ戻る</a></p>
-            </div><!-- topButton -->
-        </div>
-    </div><!-- inner -->
-</div><!-- errorSection -->
 
 
 <?php get_footer(); ?>
--- page-system.php
+++ page-system.php
@@ -1,27 +1,4 @@
 <?php get_header(); ?>
-<!-- systemSection -->
-<section class="systemSection">
-    <!-- inner -->
-    <div class="inner">
-        <!-- systemTitle -->
-        <div class="systemTitle">
-            <h2>webシステム開発</h2>
-        </div>
-        <!-- contactTitle -->
-        <div class="systemWrap">
-            <!-- subContents -->
-            <div class="systemContents">
-                <div class="systemImg">
-                    <img src="<?php echo get_template_directory_uri(); ?>/./img/systemImg_1.jpg" alt="システム開発">
-                </div>
-                <p>CMS・システム開発
-                    wordpressなど、お客さまご自身で管理・運営できるCMSの導入に柔軟に対応いたします。確かな実装力と、豊富な知見を生かし、最適なシステムをご提供いたします。自社サービス開発の経験を生かしたアプリ開発もお任せください。</p>
-            </div>
-            <aside class="subContents">
-                <?php get_sidebar("business"); ?>
-            </aside><!-- subContents -->
-        </div><!-- systemWrap -->
-    </div><!-- inner -->
-</section><!-- systemSection -->
+
 
 <?php get_footer(); ?>
